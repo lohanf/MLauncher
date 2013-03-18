8b8a5b706b257b9e29f90bd2531c24969b612a94
Florin Lohan <florin.lohan@mlauncher.org>
2013-03-18 22:35:38 +0200

    Italian added

--- sis/makesis.bat
+++ sis/makesis.bat
@@ -1,11 +1,25 @@
+REM set SAVEPATH=%PATH%
+REM cd ..\group
+
+REM set EPOCROOT=\Symbian\9.1\S60_3rd_MR\
+REM set PATH=%EPOCROOT%\epoc32\tools;%SAVEPATH%
+REM call abld clean
+REM call abld build gcce urel
+REM set EPOCROOT=\S60\devices\S60_3rd_FP2_SDK_v1.1\
+REM set PATH=%EPOCROOT%\epoc32\tools;%SAVEPATH%
+REM call abld clean
+REM call abld build gcce urel
+REM cd ..\sis
+
 del cert-gen.cer
 del key-gen.key
 
-REM makekeys -cert -expdays 3650 -password yourPw -len 2048 -dname "CN=Your Name C=FI EM=you@somewhere" fl_key.key fl_cert.cer
-createsis sign   -cert fl_cert.cer -key fl_key.key -pass yourPw OggPlayController.sis OggPlayController.sisx
-createsis create -cert fl_cert.cer -key fl_key.key -pass yourPw Mlauncher_NoSisInside.pkg 
-createsis create -cert fl_cert.cer -key fl_key.key -pass yourPw Mlauncher.pkg
+REM makekeys -cert -expdays 3650 -password HarryPotter -len 2048 -dname "CN=Florin Lohan C=FI EM=florin.lohan@mlauncher.org" fl_key.key fl_cert.cer
+createsis sign   -cert fl_cert.cer -key fl_key.key -pass HarryRonHermione OggPlayController.sis OggPlayController.sisx
+createsis create -cert fl_cert.cer -key fl_key.key -pass HarryRonHermione Mlauncher_NoSisInside.pkg 
+createsis create -cert fl_cert.cer -key fl_key.key -pass HarryRonHermione Mlauncher.pkg
 
+REM set PATH=%SAVEPATH%
 
 rename Mlauncher.SIS MLauncher_sis.sisx
 rename Mlauncher_NoSisInside.SIS MLauncher.sisx
